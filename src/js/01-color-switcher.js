7c34dacbac2b34247e7739684ec12b2cf2e93fd3
havryliaksv <gavriliaks@gmail.com>
2023-05-24 16:55:11 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const INTERVAL = 1000;
+let intervalId;
 
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
+
+refs.btnStart.addEventListener('click', onBtnStartClick);
+
+function onBtnStartClick(e) {
+  e.target.disabled = true;
+  e.target.removeEventListener('click', onBtnStartClick);
+  changeBackgroundBody();
+  intervalId = setInterval(changeBackgroundBody, INTERVAL);
+  refs.btnStop.addEventListener('click', onBtnStopClick);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function changeBackgroundBody() {
+  document.body.style.backgroundColor = getRandomHexColor();
+}
+
+function onBtnStopClick() {
+  refs.btnStart.disabled = false;
+  clearInterval(intervalId);
+  refs.btnStart.addEventListener('click', onBtnStartClick);
+  refs.btnStop.removeEventListener('click', onBtnStopClick);
+}
